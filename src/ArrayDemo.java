34c7a7ddc387f4da010b8d6fab116f23185b2a14
Alex Rauenzahn <xandermv@yahoo.com>
2019-11-10 12:12:37 -0700

    Revised

--- src/ArrayDemo.java
+++ src/ArrayDemo.java
@@ -4,11 +4,11 @@ public class ArrayDemo {
     int sample[] = new int[10];
     int i;
 
-    for (i = 0; i < 10; i = i+1) {
+    for (i = 0; i < 10; i = i+1)
       sample[i] = i;
-      for (i = 0; i < 10; i = i +1){
-        System.out.println("");
+      for (i = 0; i < 10; i = i +1)
+        System.out.println("This is sample [" + i + "]: " + sample[i]);
       }
     }
-  }
-}
+
+
